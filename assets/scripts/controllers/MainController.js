60f3c4769e0143c4838cf8a9e3b03f56bc79796d
Bill Dean <742designs@gmail.com>
2016-07-06 22:53:35 -0400

    Updating layout and removing countdown feature

--- assets/scripts/controllers/MainController.js
+++ assets/scripts/controllers/MainController.js
@@ -4,29 +4,29 @@
 
   var MainController = function($scope, $interval, $location) {
 
-    var decrementCountdown = function(){
-      $scope.countdown -= 1;
-      if ($scope.countdown < 1){
-        $scope.search($scope.username);
-      }
-    };
+    // var decrementCountdown = function(){
+    //   $scope.countdown -= 1;
+    //   if ($scope.countdown < 1){
+    //     $scope.search($scope.username);
+    //   }
+    // };
 
-    var countdownInterval = null;
-    var startCountdown = function(){
-      countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
-    };
+    // var countdownInterval = null;
+    // var startCountdown = function(){
+    //   countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+    // };
 
     $scope.search = function(username) {
-      if(countdownInterval) {
-        $interval.cancel(countdownInterval);
-        $scope.countdown = null;
-      }
+      // if(countdownInterval) {
+      //   $interval.cancel(countdownInterval);
+      //   $scope.countdown = null;
+      // }
       $location.path("/user/" + username);
     };
 
     $scope.username = "angular";
     $scope.countdown = 10;
-    startCountdown();
+    //startCountdown();
 
   };
 
