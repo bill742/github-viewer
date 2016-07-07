90c8a28c632cb2c2b726b961888261c8ebc4204a
Bill Dean <742designs@gmail.com>
2016-07-06 23:12:26 -0400

    updating template paths

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,15 +5,15 @@
   app.config(function($routeProvider){
     $routeProvider
       .when("/main", {
-        templateUrl: "/templates/main.html",
+        templateUrl: "../../templates/main.html",
         controller: "MainController"
       })
       .when("/user/:username", {
-        templateUrl: "/templates/user.html",
+        templateUrl: "../../templates/user.html",
         controller: "UserController"
       })
       .when("/repo/:username/:reponame", {
-        templateUrl: "/templates/repo.html",
+        templateUrl: "../../templates/repo.html",
         controller: "RepoController"
       })
       .otherwise({redirectTo:"/main"});
